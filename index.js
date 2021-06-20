3352e5e255df1635143b7d807f69b9db4187faa4
Your Name <you@example.com>
2021-06-20 18:48:02 +0300

    init

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const github = require("@actions/github");
 
 async function run() {
 	try {
-		const repo = github.context.repo.split("/");
 		const octokit = github.getOctokit(core.getInput("token"));
 		const issueNumber = github.context.payload.issue.number;
 
 
 		const { data: comment } = await octokit.rest.issues.createComment({
-			owner: repo[0],
-			repo: repo[1],
+			owner: github.payload.repository.owner.login,
+			repo: github.payload.repository.name,
 			issue_number: issueNumber,
 			body: "test",
 		});
